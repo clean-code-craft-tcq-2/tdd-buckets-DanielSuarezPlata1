fbbd89f4fb3b666eb2864339a25734f1b68b40fa
Suarez Plata Daniel (RBEI/EBS11-SX) <Daniel.SuarezPlata@mx.bosch.com>
2022-03-09 17:36:27 -0600

    Added: C# Project and main workflow

---
+++ RangeNumberReader/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RangeNumberReader
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public static int FetchNumberOfReadings(List<int> listOfNumbers,int rangeFrom, int rangeTo)
+        {
+
+
+            return 1;
+        }
+    }
+}
---
+++ RangeNumberReaderTests/ProgramTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RangeNumberReader;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RangeNumberReader.Tests
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void FetchNumberOfReadingsTest()
+        {
+            List<int> listOfNumbers = new List<int> {3,3,5,4,10 };
+
+            int numberOfReadings = Program.FetchNumberOfReadings(listOfNumbers, 4, 5);
+
+            Assert.AreEqual(numberOfReadings, 2);
+        }
+    }
+}
