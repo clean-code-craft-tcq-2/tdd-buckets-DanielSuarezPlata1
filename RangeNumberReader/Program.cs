4fa5635196a9aa4247468702b2a0e9182ce211c9
Suarez Plata Daniel (RBEI/EBS11-SX) <Daniel.SuarezPlata@mx.bosch.com>
2022-03-09 18:44:54 -0600

    Exclude Main from Coverage

--- RangeNumberReader/Program.cs
+++ RangeNumberReader/Program.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 
 namespace RangeNumberReader
 {
     public class Program
     {
+        [ExcludeFromCodeCoverage]
         static void Main(string[] args)
         {
             
